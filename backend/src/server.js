25c452f1246e3e52bb7c63ab6a6b6b0e53000ec1
Rohit Kumar <rohitkumar.ak51@gmail.com>
2025-07-05 15:03:59 +0100

    delete favorite

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,13 +14,13 @@ app.get("/api/health", (req, res) => {
 
 app.post("/api/favorites", async (req, res) => {
   try {
-    const { userId, recipeId, title, image, cookTime,servings } = req.body;
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
     if (!userId || !recipeId || !title) {
       return res.status(400).json({ error: "Missing required fields" });
     }
 
-    const newFavourite= await db
+    const newFavourite = await db
       .insert(favouriteTable)
       .values({
         userId,
@@ -35,6 +35,25 @@ app.post("/api/favorites", async (req, res) => {
     res.status(201).json(newFavourite[0]);
   } catch (error) {
     console.log("Error adding favourite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favouriteTable)
+      .where(
+        and(
+          eq(favouriteTable.userId, userId),
+          eq(favouriteTable.recipeId, parseInt(recipeId))
+        )
+      );
+    res.status(200).json({ message: "Favorite removed successfully" });
+  } catch (error) {
+    console.log("Error removing a favourite", error);
     res.status(500).json({ error: "Something went wrong" });
   }
 });
