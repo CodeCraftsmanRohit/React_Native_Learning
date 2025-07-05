76459b7beab2c60bc05994004ebf855feeac05ee
Rohit Kumar <rohitkumar.ak51@gmail.com>
2025-07-05 14:43:52 +0100

    add favorite route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,44 @@
-import express from "express"
-import "dotenv/config"
-import {ENV} from './config/env.js'
-const app=express();
-const PORT=ENV.PORT ||8001
+import express from "express";
+import "dotenv/config";
+import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favouriteTable } from "./db/schema.js";
+const app = express();
+const PORT = ENV.PORT || 8001;
 
-app.get("/api/health",(req,res)=>{
-    res.status(200).json({success:true});
-})
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log("Server is running on port",PORT);
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
 
-})
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime,servings } = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const newFavourite= await db
+      .insert(favouriteTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavourite[0]);
+  } catch (error) {
+    console.log("Error adding favourite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
